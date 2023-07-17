ea6e70721d2ec4affc781bd2ed60bd605cf79d46
parthapmali <136691764+parthapmali@users.noreply.github.com>
2023-07-17 08:55:30 +0700

    Improve error handling

---
+++ garden_mountain_jungle.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    // 'Plant a Seed'
+    cout << "Planting a seed is an important process in the natural cycle of growth and regeneration..." << endl; 
+  
+    // Step 1: Till the Soil
+    cout << "Step 1: Till the soil to a depth of 8 to 12 inches." << endl;
+    
+    // Step 2: Level the Soil
+    cout << "Step 2: Level the soil with a rake until it is smooth." << endl;
+    
+    // Step 3: Spread Fertilizer
+    cout << "Step 3: Spread fertilizer over the soil according to the type of seed being planted." << endl;
+    
+    // Step 4: Plant the Seeds
+    cout << "Step 4: Plant the seeds according to the planting instructions for the type of seed being planted." << endl;
+    
+    // Step 5: Water the Soil
+    cout << "Step 5: Water the soil until it is moist beneath the surface." << endl;
+    
+    // Step 6: Add Mulch
+    cout << "Step 6: Add a layer of mulch around the seed to help retain moisture and protect against weeds." << endl;
+    
+    // Step 7: Monitor Soil Moisture
+    cout << "Step 7: Monitor the soil moisture level and water as needed to keep the soil moist." << endl;
+    
+    // Step 8: Monitor for Weeds
+    cout << "Step 8: Monitor for weeds and remove them as needed to keep the soil clear." << endl;
+    
+    // Step 9: Thin the Seedlings
+    cout << "Step 9: Thin the seedlings when necessary, so that the healthiest plants have space to grow." << endl;
+    
+    // Step 10: Monitor for Pests
+    cout << "Step 10: Monitor for pest infestations and use appropriate methods of pest control." << endl;
+    
+    // Step 11: Fertilize the Soil
+    cout << "Step 11: Fertilize the soil if needed to replenish the nutrients used by the seedlings." << endl;
+    
+    // Step 12: Harvest the Crops
+    cout << "Step 12: Harvest the crops when they are ready for harvesting." << endl;
+    
+    cout << "Congratulations, you have successfully planted a seed!" << endl;
+    
+    return 0; 
+}
